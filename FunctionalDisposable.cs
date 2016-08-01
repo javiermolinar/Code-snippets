9e0b87486e07b18ef6246dafd3a5367d855ecd25
Javier Molina <javiermolinar@live.com>
2016-08-01 13:21:57 +0200

    High order function to wrap Using of disposable methods

---
+++ FunctionalDisposable.cs
@@ -0,0 +1,14 @@
+public static class Disposable
+{
+    public static TResult Using<TDisposable, TResult>
+    (
+      Func<TDisposable> factory,
+      Func<TDisposable, TResult> fn)
+      where TDisposable : IDisposable
+    {
+        using (var disposable = factory())
+        {
+            return fn(disposable);
+        }
+    }
+}
